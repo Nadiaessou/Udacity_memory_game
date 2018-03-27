a851ac8e8a810ee040a5f9d88bf847fc73782b50
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2018-03-27 22:08:29 +0200

    Set the game logic

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
+let listOfCards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"]
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +19,129 @@ function shuffle(array) {
     return array;
 }
 
+///////////////////////////////////////////////////////////////////////////////////
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+let open = [];
+let matchedCards = 0;
+let move = 0;
+let star = 3;
+
+
+/////////////////////////////// MOVES AND STARS ///////////////////////////////
+
+let level1 = 12;
+let level2 = 20;
+let level3 = 30;
+
+
+function removeStar() {
+    $(".fa-star").last().attr("class", "fa fa-star-o");
+    star--;
+    $(".num-stars").text(String(star));
+};
+
+function updateMove() {
+    $(".moves").text(move);
+    if (move === level1 || move === level2 || move === level3) {
+        removeStar();
+    }
+};
+
+
+/////////////////////////////// LOGIC CARD ///////////////////////////////
+
+
+let clickCard = function() {
+    if (selectedCard($(this))) {
+        if (open.length === 0) {
+            openCard($(this));
+
+        } else if (open.length === 1) {
+            openCard($(this));
+            move++;
+            updateMove();
+
+            if (checkMatch()) {
+                setTimeout(setMatch, 200);
+            } else {
+                setTimeout(resetOpen, 600);
+            }
+        }
+    }
+};
+
+$(".card").click(clickCard);
+
+
+/////////////////////////////// CHECK IF CARD IS OPEN AND MATCH ///////////////////////////////
+
+
+
+function selectedCard(card) {
+    return !(card.hasClass("open") || card.hasClass("match"));
+};
+
+function checkMatch() {
+    if (open[0].children().attr("class") === open[1].children().attr("class")) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+/////////////////////////////// SET AND RESET CARD OPEN ///////////////////////////////
+
+
+function openCard(card) {
+    if (!card.hasClass("open")) {
+        card.addClass("open");
+        card.addClass("show");
+        open.push(card);
+    }
+};
+
+let resetOpen = function() {
+    open.forEach(function(card) {
+        card.toggleClass("open");
+        card.toggleClass("show");
+    });
+    open = [];
+};
+
+
+/////////////////////////////// Set card to match and display alert ///////////////////////////////
+
+
+let setMatch = function() {
+    open.forEach(function(card) {
+        card.addClass("match");
+    });
+    open = [];
+    matchedCards += 2;
+    if ($('.deck').find('.match').length === 16) {
+        swal({
+            title: 'Congratulations !',
+            text: "You won this game",
+            type: 'success',
+            showCancelButton: true,
+            confirmButtonColor: '#02b3e4',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Play again'
+        }).then((result) => {
+            if (result.value) {
+                document.location.reload()
+            }
+        })
+    }
+};
+
+
+/////////////////////////////// RESET GAME ///////////////////////////////
+
+let resetGame = function() {
+    document.location.reload()
+};
+
+$(".restart").click(resetGame);
